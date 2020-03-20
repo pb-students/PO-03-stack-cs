f041a673e82334f13c307dd7c87e89668f4c1dcf
ProgramistycznySwir <39069042+ProgramistycznySwir@users.noreply.github.com>
2020-03-20 14:19:36 +0100

    Wrzuta

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,97 @@
+﻿///Zadanie 3 - stos obiektowo
+///
+///
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stos stos1 = new Stos();
+            stos1.init();
+            Stos stos2 = new Stos();
+            stos2.init();
+
+            Console.WriteLine("Wpisz 10 liczb całkowitych (każdą potwierdź enter'em):");
+
+            ///Ten kod zapisuje 2 stosy na raz przez co stos1 jest nie naruszony jak wymaga tego zadanie
+            ///proszę pisać jeśli nie o takie rozwiązanie chodziło, wtedy spróbuję jak najszybciej się poprawić :)
+            for (int i = 0; i < 10; i++)
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                stos1.push(number);
+                stos2.push(number);
+            }
+
+            Console.WriteLine("\nWpisane wartości w odwrotnej kolejności to:");
+
+            while(!stos2.empty())
+            {
+                Console.WriteLine(stos2.top());
+                stos2.pop();
+            }
+        }
+
+        class Stos
+        {
+            const int resizeAmount = 8;
+
+            int[] values;
+            //int capacity;
+            int count;
+
+            public void pop()
+            {
+                if(count < 0)
+                    Console.WriteLine("<Stos>.pop(): Stos jest już pusty!");
+                else
+                    count--;
+            }
+            public void push(int numberOnStack)
+            {
+                if(full())
+                {
+                    values[count++] = numberOnStack;
+                }
+                else
+                {
+                    Resize();
+                    values[count++] = numberOnStack;
+                }
+            }
+            public int top()
+            {
+                return values[count-1];
+            }
+            public bool empty()
+            {
+                return (count == 0);
+            }
+            public bool full()
+            {
+                return (count == values.Length);
+            }
+            public void init()
+            {
+                values = new int[16];
+            }
+            public void destroy()
+            {
+                ///Kompletnie nie wiem po co jest ta funkcja, nie ma tu nic do posprzątania a Garbage Collector zajmie się instancją obiektu
+            }
+
+            ///Poboczna metoda która sprawia że realizacja zadania jest "suchsza"
+            void Resize()
+            {
+                int[] newValues = new int[values.Length + resizeAmount];
+                int i = 0;
+                foreach (int number in values)
+                    newValues[i++] = number;
+                values = newValues;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # object-programming-03-cs
 nadal was kocham ziomeczki (no homo)
+
+W razie problemów proszę pisać do FreeDOOM#4231 na discordzie ^w^
+
+i niech żadnemu debilowi nie przyjdzie do łba by wysyłać ten .cs do babki bez uprzedniego zedytowania go we własnym stylu i pousuwania komentarzy :<
